85daa0779902f1aecb1d39440ea32d505b4eb928
David Holmes <phoenix1988@gmail.com>
2019-12-21 13:33:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
+  array.sort!
   array.last
 end
 
 def find_min_value(array)
-  array.sort
+  array.sort!
   array.first 
 end
